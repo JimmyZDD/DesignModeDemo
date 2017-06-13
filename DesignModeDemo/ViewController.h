d63865c1a8a14ca937518c900ed9920f391b4c84
zhaodongdong <445305451@qq.com>
2017-06-13 21:47:50 +0800

    Initial Commit

---
+++ DesignModeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DesignModeDemo
+//
+//  Created by JimmyZhao on 2017/6/13.
+//  Copyright © 2017年 bihu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DesignModeDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DesignModeDemo
+//
+//  Created by JimmyZhao on 2017/6/13.
+//  Copyright © 2017年 bihu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
